8c08d8fe00ccfe725888bdf3125039d910513c93
Assem Zhunis <zhunis.assem@gmail.com>
2020-05-27 13:55:18 +0600

    Initial commit

---
+++ app/src/main/java/com/example/script/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.script;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubeThumbnailView;
+
+public class MainActivity extends YouTubeBaseActivity {
+    YouTubePlayerView youTubePlayerView;
+    Button button;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.bn);
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player_view);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("3LiubyYpEUk");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize(PlayerConfig.API_KEY,onInitializedListener);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/script/PlayerConfig.java
@@ -0,0 +1,9 @@
+package com.example.script;
+
+public class PlayerConfig {
+
+    PlayerConfig(){
+
+    }
+    public static final String API_KEY = "AIzaSyAS34ss0TCfj8aJTjtqvycha8AL06Cqkjo";
+}
